1619ad04e7e55ffac39333411922cc5542ba4d8f
sjfricke <sjfricke@wisc.edu>
2017-09-16 07:44:27 -0500

    database init

---
+++ database/read.go
@@ -0,0 +1,9 @@
+package database
+
+import (
+	"fmt"
+)
+
+func Test() {
+	fmt.Println("database")
+}
--- main.go
+++ main.go
@@ -2,8 +2,10 @@ package main
 
 import (
 	"fmt"
+	"github.com/sjfricke/wubalubadubdub/database"
 )
 
 func main() {
 	fmt.Println("hello")
+	database.Test();
 }
