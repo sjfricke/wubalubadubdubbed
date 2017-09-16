bf654cc15cf2ac340ce242fc7a6844d175771220
sjfricke <sjfricke@wisc.edu>
2017-09-16 17:22:30 -0500

    updated Create and Read

--- database/create.go
+++ database/create.go
@@ -3,15 +3,25 @@ package database
 import (
 	"database/sql"
 	"log"
+	"strings"
+	
 	// Import postgres driver.
 	_ "github.com/lib/pq"
 )
 
-func CreatePhrase(db *sql.DB, entry PhraseEntry) {
-	const insertSQL =  `
+const  _createSQL =  `
 INSERT INTO wubalubadubdub.words (phrase, file, startPhrase, endPhrase, nextPhrase) VALUES ($1, $2, $3, $4, $5);
 `
-	_, err := db.Exec(insertSQL, entry.Phrase, entry.File, entry.Start, entry.End, entry.Next)
+
+func CreatePhrase(db *sql.DB, entry PhraseEntry) {
+
+	// Keeping all words lowercase
+	// also trimming for good mesaures
+	_, err := db.Exec( _createSQL,
+		strings.ToLower(strings.TrimSpace(entry.Phrase)),
+		strings.ToLower(strings.TrimSpace(entry.File)),
+		entry.Start, entry.End, entry.Next)
+	
 	if err != nil {
 		log.Println(err)
 	}
--- database/read.go
+++ database/read.go
@@ -1,9 +1,56 @@
 package database
 
 import (
+	"database/sql"
+	"log"
+	"strings"
+//	"time"
 	"fmt"
+	
+	// Import postgres driver.
+	_ "github.com/lib/pq"
 )
 
-func Test() {
-	fmt.Println("database")
+func ReadPhrase(db *sql.DB, phr string) PhraseEntry {
+
+	rows, err := db.Query(fmt.Sprintf(`SELECT phrase, file, startPhrase, endPhrase, nextPhrase
+                                           FROM wubalubadubdub.words
+                                           WHERE (phrase LIKE '%s');`,
+                                           strings.ToLower(strings.TrimSpace(phr))))
+
+	if  err != nil {
+		log.Println(err)
+	}
+
+	// hold a temp entry and return entry
+	entry := PhraseEntry{}
+//	rEntry := PhraseEntry{}
+
+	// used to hold the longest phrase so we can get the word with longest pattern
+//	var lPhrase int = 0;
+	
+	defer rows.Close()	
+	for rows.Next() {
+		if err = rows.Scan(&entry.Phrase, &entry.File, &entry.Start, &entry.End, &entry.Next); err != nil {
+			log.Println(err)
+		}
+
+		// Currently just returning first foudn item
+		break
+		
+		// check if this row has longer span of words
+//		count := len(strings.Split(entry.Phrase, " "))
+//		if lPhrase < count {
+//			lPhrase = count
+//			rEntry.Phrase = entry.Phrase
+//			rEntry.File = entry.File
+//			rEntry.Start = entry.Start
+//			rEntry.End = entry.End
+//			rEntry.Next = entry.Next
+//		}
+	}
+
+	err = rows.Err() // get encountered errors
+
+	return entry
 }
