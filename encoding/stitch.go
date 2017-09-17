e4f2f96e7b8386b3f6acb6257330162639ae0503
sjfricke <sjfricke@wisc.edu>
2017-09-16 20:48:18 -0500

    encoding added

---
+++ encoding/stitch.go
@@ -0,0 +1,41 @@
+package encoding
+
+import (
+	"bytes"
+	"strconv"
+)
+
+func init() {
+}
+
+// Crop takes file and start and length and sets to out file
+func Stitch(inputs []string, output string) {
+	var stitchArgs []string
+
+	// add input files
+	for _, in := range inputs {
+		stitchArgs = append(stitchArgs, "-i", in)
+	}
+
+	stitchArgs = append(stitchArgs, "-filter_complex")
+
+	var filBuf bytes.Buffer
+//	filBuf.WriteString("'")
+	for i, _ := range inputs {
+		filBuf.WriteString("[")
+		filBuf.WriteString(strconv.Itoa(i))
+		filBuf.WriteString(":0][")
+		filBuf.WriteString(strconv.Itoa(i))
+		filBuf.WriteString(":1]")
+	}
+
+	filBuf.WriteString(" concat=n=")
+	filBuf.WriteString(strconv.Itoa(len(inputs)))
+	filBuf.WriteString(":v=1:a=1 [v][a]")
+	stitchArgs = append(stitchArgs, filBuf.String())
+
+	stitchArgs = append(stitchArgs, "-map", "[v]", "-map", "[a]")
+	stitchArgs = append(stitchArgs, "-strict", "-2", output);
+	
+	Ffmpeg(stitchArgs...)
+}
